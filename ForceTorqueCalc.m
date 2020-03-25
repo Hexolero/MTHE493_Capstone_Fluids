1bc520c846731b18b556f7a21370869f394115a1
Daniel <497dmac@gmail.com>
2020-03-25 11:45:28 -0400

    Daniel: ForceTorqueCalc function complete

--- ForceTorqueCalc.m
+++ ForceTorqueCalc.m
@@ -1,18 +1,17 @@
-function [force torque] = ForceTorqueCalc (controlPoints, lambda, flowFieldFnHandle, stagPressure)
+function [force, torque] = ForceTorqueCalc (controlPoints, flowFieldFnHandle, stagPressure)
 % ForceTorqueCalc calcualtes the resulting force and torques acting on the 
 % body due to the pressures forces acting on the body
-%   
 
 %% STEP 0: Define variables used throughout program
 % Number of panels in shape
 N = size(controlPoints,1); 
-% Desity of saltwater [kg/m^3]
+% Desity of saltwater [kg/m^3] (Need to find a reputable source for this)
 RHO = 1020;
 % Thickness of each panel [m](maybe should be a parameter that gets passed
 % in?)
 THICKNESS = 0.1;
 % Center of mass of shape
-COM = [sum(controlPoints(:,1)), sum(controlPoints(:,2))] / N;
+COM = sum(controlPoints) / N;
 
 %% STEP 1: Determine normal vectors for each panel
 % Normal vector pointing outward from shape for each panel
@@ -37,20 +36,41 @@ end
 
 %% STEP 3: Force due to pressure at each panel's midpoint
 % Calcualte the force due to pressure acting at the midpoint of each panel
-PressureForce = zeros(size(controlPoints));
+pressureForce = zeros(size(controlPoints));
 
 for i=1:N
     % get x and y components of velocity at midpoint of panel using
     % TotalFlow function and then calcualte corresponding pressure force.
     % Note that pressure force acts in inward-direction to shape.
     [u,v] = TotalFlow( controlPoints, lambda, flowFieldFnHandle, midpoints(i,:));
-    PressureForce(i,:) = -((stagPressure - 0.5* RHO * vecnorm([u,v])^2) * length(i) * THICKNESS) * normals(i,:);
+    pressureForce(i,:) = ((stagPressure - 0.5* RHO * vecnorm([u,v])^2) * length(i) * THICKNESS) * -normals(i,:);
 end
 
 %% STEP 4: Total force due to pressure acting at COM
-force = sum(PressureForce);
+% Gives a single force acting at the center of mass of the body
+
+force = sum(pressureForce);
 
 %% STEP 5: Torque due to pressure acting on body
+% Gives a single torque that acts about the COM of the body.
+% Recall: Torque = position x force.
+
+% NOTE 1: To use cross porduct in MATLAB, vecotr needs to be 1x3 or 3x1
+% vectors
+% NOTE 2: Need to genereate position vector (ie location force is acting 
+% relative to COM) for each pressure force. This is achieved by creating a
+% vector from the COM to the midpoint of the segment that the pressure
+% force is acting on.
+
+% position of force vectors realtive to COM with column of zeros added to
+% make the position vectors 3x1
+position = horzcat((midpoints - COM),zeros(N,1));
+
+% temporary vector to put the forces into 3x1 vectors
+tempForce = horzcat(pressureForce,zeros(N,1));
+
+% Calcualtes the cross product for total torque acting on the body. 
+torque = sum(cross(position,tempForce,2));
 
 
 
